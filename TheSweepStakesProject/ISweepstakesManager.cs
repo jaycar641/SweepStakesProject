76339d5502176c17a521f4c216e794d8a7d151f3
Jacob Carson <jaycar641@yahoo.com>
2019-10-15 17:17:52 -0500

    Skeleton and Initial Commit

---
+++ TheSweepStakesProject/ISweepstakesManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSweepStakesProject
+{
+    interface ISweepstakesManager
+    {
+        void InsertSweepstake(Sweepstake sweepstakes);
+
+        Sweepstake GetSweepStake();
+
+    }
+}
---
+++ TheSweepStakesProject/Sweepstake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSweepStakesProject
+{
+    class Sweepstake
+    {
+        public string name;
+        public Sweepstake()
+        {
+            this.name = name;
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+
+        }
+
+        public Contestant PickWinner()
+        {
+
+            return;
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+
+    }
+}
